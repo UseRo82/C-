9e4b122a5bd4799a4adcbcdc7acee9fcf86598bd
UseRo82 <110923451+UseRo82@users.noreply.github.com>
2022-08-25 15:57:50 +0200

    Add files via upload

---
+++ ArraysParametros/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArraysParametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[4];
+            numeros[0] = 7;
+            numeros[1] = 2;
+            numeros[2] = 5;
+            numeros[3] = 4;
+
+            ProcesaDatos(numeros);
+
+            foreach(int i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void ProcesaDatos(int[] datos)
+        {
+            foreach(int i in datos)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Añadiendo datos al array.
+            for(int i = 0; i < 4; i++)
+            {
+                datos[i] += 10;
+            }
+        }
+    }
+}
