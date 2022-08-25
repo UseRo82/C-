fa1336f3b904946f922f52545633c86d28348cd3
UseRo82 <110923451+UseRo82@users.noreply.github.com>
2022-08-25 16:18:58 +0200

    Add files via upload

---
+++ Destructor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Destructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos miArchivo = new ManejoArchivos();
+
+            miArchivo.mensaje();
+        }
+    }
+    class ManejoArchivos
+    {
+        StreamReader archivo = null;
+
+        int contador = 0;
+        string linea;
+
+        public ManejoArchivos()
+        {
+            archivo = new StreamReader(@"C:\Users\rodri\Desktop\prueba.txt");
+
+            while((linea = archivo.ReadLine()) != null)
+            {
+                Console.WriteLine(linea);
+
+                contador++;
+            }
+        }
+        public void mensaje()
+        {
+            Console.WriteLine("Hay {0} líneas.", contador);
+        }
+        //Destructor. Para cerrar el archivo.
+        ~ManejoArchivos()
+        {
+            archivo.Close();
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> edades = new Dictionary<string, int>();
+
+            //Rellenando el diccionario.
+
+            edades.Add("Felipe", 76);
+            edades.Add("Javier", 65);
+
+            edades["María"] = 25;
+            edades["Juan"] = 62;
+
+            //Recorriendo diccionario.
+            foreach(KeyValuePair<string, int> persona in edades)
+            {
+                Console.WriteLine("Nombre: {0} Edad {1}", persona.Key, persona.Value);
+            }
+        }
+    }
+}
